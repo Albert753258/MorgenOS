c508383b7c9f17f52a905d8e106734a6be2c1fc9
Albert <albert.nasyrov2016@gmail.com>
2021-06-24 01:05:14 +0300

    Snus

--- MorgenOS.asm
+++ MorgenOS.asm
@@ -1,25 +1,72 @@
-section .text
-     use16
-     org  0x7C00  ;
+use16
+org 0x7c00
 start:
-     mov  ax, cs
-     mov  ds, ax  ;
+  jmp 0x0000:entry              ;
+entry:
+  mov ax, cs
+  mov ds, ax
  
-     mov  si, message
-     cld              ;
-     mov  ah, 0x0E    ;
-     mov  bh, 0x00    ;
-puts_loop:
-     lodsb            ;
-     test al, al      ;
-     jz   puts_loop_exit
-     int  0x10        ;
-     jmp  puts_loop
-puts_loop_exit:
-     jmp  $           ;
+
+  mov ax, 0x0003
+  int 0x10
  
-message:
-     db   'First beta MorgenOS release', 0
+
+  in  al, 0x92
+  or  al, 2
+  out 0x92, al
+ 
+
+  lgdt  [gdtr]
+
+  cli
+
+  in  al, 0x70
+  or  al, 0x80
+  out 0x70, al
+ 
+
+  mov  eax, cr0
+  or   al, 1
+  mov  cr0, eax
+ 
+
+  O32 jmp 00001000b:pm_entry
+ 
+
+use32
+
+pm_entry:
+  mov  ax, cs
+  mov  ds, ax
+  mov  es, ax
+ 
+  mov  edi, 0xB8000
+  mov  esi, msg
+  cld
+.loop
+  lodsb
+  test al, al
+  jz   .exit
+  stosb
+  mov  al, 7
+  stosb
+  jmp  .loop
+.exit
+ 
+  jmp  $
+ 
+msg:
+  db  'First beta MorgenOS release', 0
+ 
+gdt:
+  db  0x00, 0x00, 0x00, 0x00, 0x00,      0x00,      0x00, 0x00 
+  db  0xFF, 0xFF, 0x00, 0x00, 0x00, 10011010b, 11001111b, 0x00
+gdt_size  equ $ - gdt
+
+gdtr:
+  dw  gdt_size - 1
+  dd  gdt
+ 
 finish:
-     times 0x1FE-finish+start db 0
-     db   0x55, 0xAA  ;
+times 0x1FE-finish+start db 0
+db   0x55, 0xAA
